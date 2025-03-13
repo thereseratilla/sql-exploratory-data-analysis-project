acc4bde0a83294c4625b1f4780c22d8aa4ba2cc6
thereseratilla <therese.ratilla@gmail.com>
2025-03-13 16:08:56 +0800

    Update 01_database_exploration.sql

--- scripts/01_database_exploration.sql
+++ scripts/01_database_exploration.sql
@@ -1,13 +1,17 @@
 /*------------------------------------------------------------------------------------
 1. DATABASE EXPLORATION
--------------------------------------------------------------------------------------*/
+-------------------------------------------------------------------------------------
+Purpose: 
+  - To explore the structure of the database, i.e., tables and schemas
+  - To inspect the columns within specific tables
+*/
 
 -- 1.1 Explore Objects in the Database: Check the metadata
 
 SELECT *
 FROM INFORMATION_SCHEMA.TABLES ;
 
--- 1.2 Explore All Columns in the Database: the structure, naming, type, etc.
+-- 1.2 Explore All columns in the database for the products table
 
 SELECT *
 FROM INFORMATION_SCHEMA.COLUMNS
