b373f84405dd1d5b71094815954b37ed318c7f17
thereseratilla <therese.ratilla@gmail.com>
2025-03-13 16:03:05 +0800

    Create 02_dimensions_exploration.sql

---
+++ scripts/02_dimensions_exploration.sql
@@ -0,0 +1,19 @@
+
+/*------------------------------------------------------------------------------------
+2. DIMENSION EXPLORATION
+-------------------------------------------------------------------------------------*/
+
+-- 2.1. Identify unique values in each dimension
+
+------ Where do our customers come from?
+
+SELECT DISTINCT country
+FROM gold.dim_customers;
+
+------ What are the product categories, subcategories and product name in the business?
+
+SELECT	 DISTINCT category
+		,subcategory
+		,product_name
+FROM gold.dim_products
+ORDER BY 1,2,3;
